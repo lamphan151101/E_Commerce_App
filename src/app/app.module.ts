8bf348a9f101b27ab214698493dff97e27b3f2ec
lamphan1 <lamphan.151101@gmail.com>
2024-01-26 23:06:19 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { OrderComponent } from './order/order.component';
+import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { DetailProductComponent } from './detail-product/detail-product.component';
 
-import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    OrderComponent,
+    OrderConfirmComponent,
+    LoginComponent,
+    RegisterComponent,
+    DetailProductComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    // HomeComponent,
+    // OrderComponent,
+    // OrderConfirmComponent,
+    // LoginComponent,
+    // RegisterComponent,
+    DetailProductComponent
+  ]
 })
 export class AppModule { }
